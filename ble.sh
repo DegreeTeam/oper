0b3df5bae789e1155f49f9b2d9f1b01f4b203b8a
root <root@pi.(none)>
2015-06-03 11:59:44 +0000

    network renewal

--- ble.sh
+++ ble.sh
@@ -4,4 +4,4 @@ sudo hciconfig hci0 up
 sudo hciconfig hci0 name test
 sudo hciconfig hci0 noscan
 sudo hciconfig hci0 leadv 3
-sudo hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 74 65 73 74 74 65 73 74 31 32 33 34 35 36 37 38 33 00 00 00 c5 00
+sudo hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 64 69 73 70 6c 61 79 31 31 32 33 34 35 36 37 38 33 00 00 00 c5 00
--- setting.sh
+++ setting.sh
@@ -1,7 +1,7 @@
 sudo service hostapd restart
 sudo bash /home/pi/oper/ble.sh
-sudo echo nameserver 8.8.8.8 nameserver 8.8.4.4 > /etc/resolv.conf
+sudo echo nameserver 8.8.8.8 > /etc/resolv.conf
+sudo echo nameserver 8.8.4.4 >> /etc/resolv.conf
 sudo modprobe ipt_MASQUERADE
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo /home/pi/HamProject/jangtest3
-sudo /home/pi/oper/manage
+sudo /home/pi/oper/streaming &
